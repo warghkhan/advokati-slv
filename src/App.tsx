712c4c44ff17d7f211d5bfe1a03529b89a9c47d6
Zed Damasky <a.yambulatov@yandex.ru>
2026-03-02 04:47:02 +0400

    update fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 function App() {
   return (
-    <div className="">
-      <h1 className="flex justify-center text-9xl font-merriweather">Test header</h1>
-      <div className="flex justify-center text-3xl font-roboto-flex">Test content</div>
+    <div className="flex flex-col gap-5 top-2 relative">
+      <h1 className="flex justify-center text-6xl font-headers uppercase font-bold">Заголовки секций</h1>
+      <div className="flex justify-center text-4xl font-sub-headers">Подзаголовки</div>
+      <div className="flex justify-center text-3xl font-main-content">Основной контент</div>
     </div>
   );
 }
