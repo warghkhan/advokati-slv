7b38e76cf0486d0298bdd750e29ed72fa8048b47
Zed Damasky <a.yambulatov@yandex.ru>
2026-03-02 04:51:02 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ function App() {
   return (
     <div className="flex flex-col gap-5 top-2 relative">
       <h1 className="flex justify-center text-6xl font-headers uppercase font-bold">Заголовки секций</h1>
-      <div className="flex justify-center text-4xl font-sub-headers">Подзаголовки</div>
-      <div className="flex justify-center text-3xl font-main-content">Основной контент</div>
+      <div className="flex justify-center text-4xl font-sub-headers font-medium">Подзаголовки</div>
+      <div className="flex justify-center text-3xl font-main-content font-medium">Основной контент</div>
+      <div className="flex justify-center text-1xl font-main-content font-light italic">Дополнительный контент</div>
     </div>
   );
 }
